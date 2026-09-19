526998cfbc971fcc138227f5f26f1cfe63fcea79
Yessir30000 <authorofzeke@gmail.com>
2026-09-19 12:54:40 -0400

    Clean re-init: proper repo scope

---
+++ src/main/java/org/compass/vc_compass/VcCompassApplication.java
@@ -0,0 +1,13 @@
+package org.compass.vc_compass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcCompassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcCompassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/compass/vc_compass/VcCompassApplicationTests.java
@@ -0,0 +1,13 @@
+package org.compass.vc_compass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcCompassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
